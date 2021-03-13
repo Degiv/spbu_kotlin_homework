11974ea579dbf05de94dd60b02ec0d4d0a343e66
Ivan Degtyarev <st051982@student.spbu.ru>
2021-03-14 01:31:53 +0300

    hw1.1 added

---
+++ src/main/kotlin/hw1/task2/main.kt
@@ -0,0 +1,27 @@
+package hw1.task2
+
+fun getFactorialIterative(x: Int): Int {
+    var result = 1
+    for (i in 2..x) {
+        result *= i
+    }
+    return result
+}
+
+fun getFactorialRecursive(x: Int): Int {
+    if (x < 2) {
+        return 1
+    }
+    return getFactorialRecursive(x - 1) * x
+}
+
+fun main() {
+    print("Enter a positive integer number: ")
+    var x = readLine()?.toIntOrNull()
+    while (x == null || x < 0) {
+        print("Incorrect input. Try again:")
+        x = readLine()?.toIntOrNull()
+    }
+    println("Factorial of your number by iterative method: ${getFactorialIterative(x)}")
+    println("Factorial of your number by recursive method: ${getFactorialRecursive(x)}")
+}
