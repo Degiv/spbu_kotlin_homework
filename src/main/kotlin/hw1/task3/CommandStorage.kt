ec227834848a3c332f54ef21392b9690ffde6d20
Ivan Degtyarev <st051982@student.spbu.ru>
2021-03-24 05:24:02 +0300

    hw2.1 added

--- src/main/kotlin/hw1/task3/CommandStorage.kt
+++ src/main/kotlin/hw1/task3/CommandStorage.kt
@@ -1,13 +1,22 @@
 package hw1.task3
 
+/**
+ * Class for storing a list of numbers and performed actions on it
+ */
 class CommandStorage {
     val data = mutableListOf<Int>()
     val commandList = mutableListOf<Action>()
 
+    /**
+     * Adds the action to list of performed actions
+     */
     fun addAction(newAction: Action) {
         commandList.add(newAction)
     }
 
+    /**
+     * Undoes last performed action
+     */
     fun undoLastAction() {
         if (commandList.isEmpty()) {
             println("Nothing to undo")
