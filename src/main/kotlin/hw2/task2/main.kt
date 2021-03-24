bf6a5a2ae20dd2a94223dbdfd7c41c5ca3edda04
Ivan Degtyarev <st051982@student.spbu.ru>
2021-03-24 08:05:19 +0300

    hw2.2 added

---
+++ src/main/kotlin/hw2/task2/main.kt
@@ -0,0 +1,22 @@
+package hw2.task2
+
+/**
+ * inputs list of integers
+ * @param {String} [prompt] - user input prompt
+ * @return list of integers]]
+ */
+fun inputIntList(prompt: String = "") : MutableList<Int> {
+    println(prompt)
+    val numbersAsStrings : List<String> = readLine().toString().split(" ")
+    val numbers = mutableListOf<Int>()
+    for(numberAsString in numbersAsStrings) {
+        val number = numberAsString.toIntOrNull() ?: error("Incorrect input.")
+        numbers.add(number)
+    }
+    return numbers
+}
+
+fun main() {
+    val numbers = inputIntList("Enter your array of numbers in one line: ")
+    print("Your array without duplicates: ${numbers.asReversed().distinct().asReversed()}")
+}
