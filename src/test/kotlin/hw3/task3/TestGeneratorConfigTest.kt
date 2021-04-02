638065fc94399fd32429767de194004c0c71a576
Ivan Degtyarev <st051982@student.spbu.ru>
2021-04-02 21:31:20 +0300

    TestGeneratorTest, TestGeneratorConfigTest

--- src/test/kotlin/hw3/task3/TestGeneratorConfigTest.kt
+++ src/test/kotlin/hw3/task3/TestGeneratorConfigTest.kt
@@ -5,12 +5,12 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-internal class TestGeneratorTest {
+internal class TestGeneratorConfigTest {
     companion object {
         @JvmStatic
         fun inputData(): List<Arguments> = listOf(
             Arguments.of(
-                "testGenerator1.kt",
+                "testConfig1.yaml",
                 TestGeneratorConfig(
                     "hw1.task3",
                     "InsertForward",
@@ -21,7 +21,7 @@ internal class TestGeneratorTest {
                 )
             ),
             Arguments.of(
-                "testGenerator2.kt",
+                "testConfig2.yaml",
                 TestGeneratorConfig(
                     "hw1.task3",
                     "InsertBack",
@@ -36,10 +36,7 @@ internal class TestGeneratorTest {
 
     @MethodSource("inputData")
     @ParameterizedTest(name = "{index} {0}")
-    fun getFile(expectedName: String, testGeneratorConfig: TestGeneratorConfig) {
-        assertEquals(
-            javaClass.getResource(expectedName).readText(),
-            TestGenerator(testGeneratorConfig).file.toString()
-        )
+    fun getFromYamlTest(inputName: String, expectedConfig: TestGeneratorConfig) {
+        assertEquals(expectedConfig, TestGeneratorConfig.getFromYaml(javaClass.getResource(inputName).readText()))
     }
 }
---
+++ src/test/resources/hw3/task3/testGenerator2.kt
@@ -0,0 +1,13 @@
+package hw1.task3
+
+import org.junit.jupiter.api.Test
+
+internal class InsertBackTest {
+  @Test
+  fun perform() {
+  }
+
+  @Test
+  fun undo() {
+  }
+}
