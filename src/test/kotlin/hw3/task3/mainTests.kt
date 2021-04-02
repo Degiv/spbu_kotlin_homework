e89f0a3a7133455304a8a0ce0b2ca9bf211ac3e3
Ivan Degtyarev <st051982@student.spbu.ru>
2021-04-02 23:48:08 +0300

    try to fix tempdir problems

--- src/test/kotlin/hw3/task3/mainTests.kt
+++ src/test/kotlin/hw3/task3/mainTests.kt
@@ -6,29 +6,29 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.nio.file.Path
+import kotlin.io.path.pathString
 
 internal class MainTests {
-  companion object {
-    @JvmStatic
-    fun inputData(): List<Arguments> = listOf(
-      Arguments.of(
-        "test1",
-        "test1/Stack.kt"
-      ),
-      Arguments.of(
-        "test2",
-        "test2/Person.kt"
-      ),
-    )
-  }
 
-  @MethodSource("inputData")
-  @ParameterizedTest(name = "{index} {0}")
-  fun getFile(directoryName: String, tempFileName: String, @TempDir tempDirectory: Path) {
-    val expectedCode = javaClass.getResource("mainTests/$directoryName/expected.kt").readText()
-    val config = javaClass.getResource("mainTests/$directoryName/config.yaml").path
-    val file = tempDirectory.resolve(tempFileName)
-    generateTestFile(config, tempDirectory.toString())
-    assertEquals(expectedCode.replace("\r\n", "\n"), file.toFile().readText().replace("\r\n", "\n"))
-  }
+    companion object {
+        @JvmStatic
+        fun inputData(): List<Arguments> = listOf(
+            Arguments.of(
+                "test1", "Stack.kt"
+            ),
+            Arguments.of(
+                "test2", "Person.kt"
+            )
+        )
+    }
+
+    @MethodSource("inputData")
+    @ParameterizedTest(name = "{index}, {0}")
+    fun generateTestFile(directoryName: String, tempFileName: String, @TempDir tempDir: Path) {
+        val expectedCode = javaClass.getResource("mainTests/$directoryName/expected.kt").readText()
+        val config = javaClass.getResource("mainTests/$directoryName/config.yaml").path
+        val file = tempDir.resolve(tempFileName)
+        generateTestFile(config, file.toString())
+        assertEquals(expectedCode, file.toFile().readText())
+    }
 }
