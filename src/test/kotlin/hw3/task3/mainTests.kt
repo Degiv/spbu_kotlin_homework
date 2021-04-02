29d258f381c2c1d2280004c40f722b26d49c1553
Ivan Degtyarev <st051982@student.spbu.ru>
2021-04-03 00:02:15 +0300

    back to normal pathes

--- src/test/kotlin/hw3/task3/mainTests.kt
+++ src/test/kotlin/hw3/task3/mainTests.kt
@@ -14,21 +14,21 @@ internal class MainTests {
         @JvmStatic
         fun inputData(): List<Arguments> = listOf(
             Arguments.of(
-                "test1", "Stack.kt"
+                "test1", "test1/Stack.kt"
             ),
             Arguments.of(
-                "test2", "Person.kt"
+                "test2", "test2/Person.kt"
             )
         )
     }
 
     @MethodSource("inputData")
-    @ParameterizedTest(name = "{index}, {0}")
+    @ParameterizedTest(name = "{index} {0}")
     fun generateTestFile(directoryName: String, tempFileName: String, @TempDir tempDir: Path) {
         val expectedCode = javaClass.getResource("mainTests/$directoryName/expected.kt").readText()
         val config = javaClass.getResource("mainTests/$directoryName/config.yaml").path
         val file = tempDir.resolve(tempFileName)
-        generateTestFile(config, file.toString())
+        generateTestFile(config, tempDir.toString())
         assertEquals(expectedCode, file.toFile().readText())
     }
 }
