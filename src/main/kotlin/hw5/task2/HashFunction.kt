82ca026151dc0fa2d288b5be7093688256bf1222
Ivan Degtyarev <st051982@student.spbu.ru>
2021-04-16 17:13:19 +0300

    HashFunction

--- src/main/kotlin/hw5/task2/HashFunction.kt
+++ src/main/kotlin/hw5/task2/HashFunction.kt
@@ -1,5 +1,33 @@
 package hw5.task2
 
 interface HashFunction<K> {
-    fun getHash(key: K): Int
+    fun getHash(key: K, mod: Int): Int
+}
+
+class PolynomialHashFunction : HashFunction<String> {
+    companion object {
+        const val QUOTIENT = 31
+    }
+    override fun getHash(key: String, mod: Int): Int {
+        val QUOTIENT = 31
+        var result = 0
+        key.toCharArray().forEach { result = (result * QUOTIENT + it.toInt()) % mod }
+        return result
+    }
+}
+
+class SumHashFunction : HashFunction<String> {
+    override fun getHash(key: String, mod: Int): Int {
+        var result = 0
+        key.toCharArray().forEach { result = (result + it.toInt()) % mod }
+        return result
+    }
+}
+
+class XorHashFunction : HashFunction<String> {
+    override fun getHash(key: String, mod: Int): Int {
+        var result = 0
+        key.toCharArray().forEach { result = result xor it.toInt() }
+        return result % mod
+    }
 }
