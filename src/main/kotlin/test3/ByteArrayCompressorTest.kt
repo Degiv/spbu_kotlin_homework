34aff6f71cb84ebdb92f5311c3c88230dc851d64
Ivan Degtyarev <st051982@student.spbu.ru>
2021-06-04 13:58:37 +0300

    save point

--- src/main/kotlin/test3/ByteArrayCompressorTest.kt
+++
@@ -1,4 +0,0 @@
-package test3
-
-class ByteArrayCompressorTest {
-}
---
+++ src/test/kotlin/test3/ByteArrayCompressorTest.kt
@@ -0,0 +1,15 @@
+package test3
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+companion object {
+    @JvmStatic
+    fun generalTests(): List<Arguments> = listOf(
+        Arguments.of()
+    )
+}
+
+class ByteArrayCompressorTest {
+}
